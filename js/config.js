aee8c4f95a35636447c7015b322b47fcf7db878e
sociodigitallimasierra <sociodigitallimasierra@gmail.com>
2026-09-02 17:07:52 -0300

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@ const CONFIG = {
   adminPassword: "cga4233Qwee",
   googleSheet: {
     sheetId: "15-VnTEYhJN_FEBFZ3YQae2v_tIyYC3rowDHR5E2x-KY",
-    appsScriptUrl: "https://script.google.com/macros/s/AKfycbxQkKYveXxMIyIOBiwgs1fpLA8vJWPtzLZ0c4TQlckX_nTLDwy8j2ajwtDMlG_fAmWd/exec",
+    appsScriptUrl: "https://script.google.com/macros/s/AKfycbwhtRm9IoGYus1sGDhdhQh7pHb5rQXafdbxWDByGoKKmk2Do6r0dPXWZXc4IJMqGt-0/exec",
     productsSheet: "Products",
     salesSheet: "Sales"
   },
